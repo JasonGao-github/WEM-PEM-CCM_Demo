b0e15d995b28b81ec8caa914f399724553fd74c4
jason-gx1989 <jason-gx1989@users.noreply.github.com>
2021-08-07 16:59:10 +1000

    test

--- src/main/java/com/wemccm/App.java
+++ src/main/java/com/wemccm/App.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class App {
 
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
