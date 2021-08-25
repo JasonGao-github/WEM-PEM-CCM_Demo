07cb53ac0843dfdc19e2d7851e9888de81db8db2
909858512@qq.com <909858512@qq.com>
2021-08-25 21:41:53 +0800

    Revert "8/24/Shuhao"
    This reverts commit 863ab7d38cdced3d015cfebd67b1e6cdedbb3a1f.

--- src/main/java/com/wemccm/demo/DemoController.java
+++ src/main/java/com/wemccm/demo/DemoController.java
@@ -1,22 +1,18 @@
-package com.wemccm.findPage.controller;
+package com.wemccm.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wemccm.common.page.FindPageRequestDto;
-import com.wemccm.common.page.PageResult;
-import com.wemccm.findPage.service.FindPageService;
-
-@RestController 
-public class FindPageController {
+@RestController
+public class DemoController {
 
 	@Autowired
-	private FindPageService service;
+	private DemoService service;
 
 	@RequestMapping(value = "/findPage", produces = "application/json;charset=UTF-8")
-	public PageResult findPage(@RequestBody FindPageRequestDto dto) {
+	public Object findPage(@RequestBody DemoPageRequestDto dto) {
 
 		// DemoPageRequestDto继承于PageRequest，包含两个分页需要的参数。
 		// PageRequest可直接作为列表查询参数对象的基类。
--- src/main/java/com/wemccm/demo/DemoMapper.java
+++ src/main/java/com/wemccm/demo/DemoMapper.java
@@ -1,20 +1,19 @@
-package com.wemccm.findPage.dao;
+package com.wemccm.demo;
 
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import com.wemccm.common.entity.User;
-import com.wemccm.common.page.FindPageRequestDto;
 
 @Mapper
-public interface FindPageDao {
+public interface DemoMapper {
 
 	/**
 	 * 分页查询用户
 	 * 
 	 * @return
 	 */
-	List<User> selectPage(FindPageRequestDto dto);
+	List<User> selectPage(DemoPageRequestDto dto);
 
 }
