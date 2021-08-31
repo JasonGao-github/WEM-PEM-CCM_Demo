ee003ad81827c97c57ec483e4a0a5d3bd6fb069c
jason-gx1989 <jason-gx1989@users.noreply.github.com>
2021-08-31 22:01:44 +1000

    commit

--- src/main/java/com/wemccm/login/controller/LoginController.java
+++ src/main/java/com/wemccm/login/controller/LoginController.java
@@ -17,28 +17,20 @@ public class LoginController {
 	@Autowired
 	private LoginService serivce;
 
-	@RequestMapping(value = "/login", produces = "application/json;charset=UTF-8")
+	@RequestMapping(value = "/loginPage", produces = "application/json;charset=UTF-8")
 	@ResponseBody
 	public ResponseResult sendMyName(@RequestBody LoginRequestPojo requestPojo) {
 
 		String email = requestPojo.getEmail();
 		String password = requestPojo.getPassword();
 
-		// 如果成功就返回user的id，失败一个返回-1，一个返回-2；
 		String s = serivce.login(email, password);
-		if ("-1".equals(s)) {
-
-			// 登录失败。。。
+		if ("01".equals(s)) {
 			return new ResponseResult(SR.REQUEST_RESULT_FAIL, SR.LOGIN_ERROR1);
 		}
-		if ("-2".equals(s)) {
-
-			// 登录失败。。
+		if ("02".equals(s)) {
 			return new ResponseResult(SR.REQUEST_RESULT_FAIL, SR.LOGIN_ERROR2);
 		}
-
-		// TODO 登录成功以后用上一步返回的userid去数据库里查找这个user对象，需要根据userTypeID把对应的userType取出来。
-		// TODO 取出来的东西，除了用户的密码以外，都放到session里。
 		return new ResponseResult();
 	}
 
