31b495d76c89d7b37271a59925eb3f44b591bda6
jason-gx1989 <jason-gx1989@users.noreply.github.com>
2021-09-04 20:13:23 +1000

    add firstname and lastname

--- src/main/java/com/wemccm/common/entity/User.java
+++ src/main/java/com/wemccm/common/entity/User.java
@@ -3,29 +3,53 @@ package com.wemccm.common.entity;
 public class User extends BaseEntity {
 
 	private String username;
+	private String firstName;
+	private String lastName;
 	private String password;
 	private String email;
 	private Integer userTypeId;
 	private Integer leaderId;
-	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Integer getUserTypeId() {
 		return userTypeId;
 	}
@@ -33,13 +57,13 @@ public class User extends BaseEntity {
 	public void setUserTypeId(Integer userTypeId) {
 		this.userTypeId = userTypeId;
 	}
+
 	public Integer getLeaderId() {
 		return leaderId;
 	}
+
 	public void setLeaderId(Integer leaderId) {
 		this.leaderId = leaderId;
 	}
-	
-	
 
 }
