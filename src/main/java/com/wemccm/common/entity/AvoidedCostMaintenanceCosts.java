2876eeb06db98141aed73dd79ee4e37fd22b7c1a
jason-gx1989 <jason-gx1989@users.noreply.github.com>
2021-09-20 15:31:41 +1000

    new task .

---
+++ src/main/java/com/wemccm/common/entity/AvoidedCostMaintenanceCosts.java
@@ -0,0 +1,96 @@
+package com.wemccm.common.entity;
+
+public class AvoidedCostMaintenanceCosts extends BaseEntity {
+
+	private Integer projectId;
+	private String name;
+	private String source;
+	private String maint;
+	private String vegMgt;
+	private Integer unit;
+	private Integer quantity;
+	private Double costPa;
+	private Double ameExist;
+	private Double avoidedCostInputTypeId;
+
+	public Integer getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(Integer projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getMaint() {
+		return maint;
+	}
+
+	public void setMaint(String maint) {
+		this.maint = maint;
+	}
+
+	public String getVegMgt() {
+		return vegMgt;
+	}
+
+	public void setVegMgt(String vegMgt) {
+		this.vegMgt = vegMgt;
+	}
+
+	public Integer getUnit() {
+		return unit;
+	}
+
+	public void setUnit(Integer unit) {
+		this.unit = unit;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getCostPa() {
+		return costPa;
+	}
+
+	public void setCostPa(Double costPa) {
+		this.costPa = costPa;
+	}
+
+	public Double getAmeExist() {
+		return ameExist;
+	}
+
+	public void setAmeExist(Double ameExist) {
+		this.ameExist = ameExist;
+	}
+
+	public Double getAvoidedCostInputTypeId() {
+		return avoidedCostInputTypeId;
+	}
+
+	public void setAvoidedCostInputTypeId(Double avoidedCostInputTypeId) {
+		this.avoidedCostInputTypeId = avoidedCostInputTypeId;
+	}
+
+}
