5c2efd14b687be5e8741ea34311e0d366b7880dd
jason-gx1989 <jason-gx1989@users.noreply.github.com>
2021-09-24 19:39:50 +1000

    fix some bugs.

--- src/main/java/com/wemccm/customercontribution/dao/CustomerContributionPriceDao.java
+++ src/main/java/com/wemccm/customercontribution/dao/CustomerContributionPriceDao.java
@@ -1,7 +1,10 @@
 package com.wemccm.customercontribution.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.wemccm.common.entity.CustomerContributionPrice;
 
+@Mapper
 public interface CustomerContributionPriceDao {
 
 	public void save(CustomerContributionPrice ccPrice);
