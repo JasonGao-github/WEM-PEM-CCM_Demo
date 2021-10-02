c1bb9ac8afbe17c666c863df078c28b64d8ad330
909858512@qq.com <909858512@qq.com>
2021-10-02 15:50:33 +0800

    10/2/Shuhao

---
+++ src/main/java/com/wemccm/contractschedule/dao/ContractScheduleDao.java
@@ -0,0 +1,25 @@
+package com.wemccm.contractschedule.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.wemccm.common.entity.NegotiatedConnection;
+import com.wemccm.common.entity.URD;
+import com.wemccm.common.pojo.NegotiatedConnectionPojo;
+import com.wemccm.common.pojo.URDPojo;
+
+@Mapper
+public interface ContractScheduleDao {
+
+
+
+	public NegotiatedConnectionPojo findNCinCC(Integer projectId);
+
+	public void insertNegotiatedConnection(NegotiatedConnection pojo);
+
+	public URDPojo findURDinCC(Integer projectId);
+
+	public void insertURD(URD pojo);
+
+}
