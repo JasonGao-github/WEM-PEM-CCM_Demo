2f351dabdcaf5604131d1d67798db23d6f4e17b0
909858512@qq.com <909858512@qq.com>
2021-10-08 00:13:33 +0800

    Update ConnectionHandoverController.java

--- src/main/java/com/wemccm/connectionhandover/controller/ConnectionHandoverController.java
+++ src/main/java/com/wemccm/connectionhandover/controller/ConnectionHandoverController.java
@@ -1,12 +1,17 @@
 package com.wemccm.connectionhandover.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.wemccm.common.entity.ConnectionHandover;
 import com.wemccm.common.entity.UplaodedFiles;
@@ -53,13 +58,22 @@ public class ConnectionHandoverController {
 
 	@RequestMapping(value = "/uplaodedFiles", produces = "application/json;charset=UTF-8")
 	@ResponseBody
-	public ResponseResult uplaodedFiles(@RequestBody UplaodedFiles Pojo) {
+	public ResponseResult uplaodedFiles(@RequestPart MultipartFile file) throws IOException  {
+		UplaodedFiles Pojo = new UplaodedFiles();
 		Integer projectId=123;
 //		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
 //				.getRequest();
 //		HttpSession session = request.getSession();
 //		projectId=(int) session.getAttribute("projectId");
 		Pojo.setProjectId(projectId);
+		String fileName = file.getOriginalFilename();
+		Pojo.setFileName(fileName);
+		String filePath ="src/main/resource/uploadfile/" + fileName;
+		Pojo.setLocalURL(filePath);
+		Pojo.setModule("ConnectionHandover");
+		File dest = new File(filePath);
+	    Files.copy(file.getInputStream(), dest.toPath());
+	     
 		serivce.uplaodedFiles(Pojo);
 		return new ResponseResult();
 	}
