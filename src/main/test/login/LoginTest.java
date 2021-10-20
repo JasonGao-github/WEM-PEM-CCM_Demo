6e13b980664608a46a827d5e67020986c5bbdb86
909858512@qq.com <909858512@qq.com>
2021-10-20 14:09:57 +0800

    test

--- src/main/test/login/LoginTest.java
+++ src/main/test/login/LoginTest.java
@@ -3,25 +3,64 @@ package login;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
 
 import com.wemccm.common.pojo.AvoidedCostassetReplacementItermDataPojo;
+import com.wemccm.login.service.LoginService;
 import com.wemccm.project.service.AvoidedCostDataService;
 
 public class LoginTest {
 
 	@Test
 //    @Disabled
-    @DisplayName("Sanity test only")
-	public void test() {
-		AvoidedCostDataService qiService;
-		int projectId = 1;
-		String projectStatus = "exist";
-		AvoidedCostassetReplacementItermDataPojo resultPojo = qiService.getData(projectId, projectStatus);
-System.out.println(resultPojo.toString());
-		assertEquals(true, true);
-	}
+//    @DisplayName("wrong email")
+	public void email() {
+		LoginService serivce=new LoginService();
+		String email = "123";
+		String password = "123";
 
+		String s = serivce.login(email, password);
+		assertEquals(s, "-1");
+	}
+	
+	
+	@Test
+//  @Disabled
+//  @DisplayName("wrong password")
+	public void password() {
+		LoginService serivce=new LoginService();
+		String email = "123";
+		String password = "123";
+
+		String s = serivce.login(email, password);
+		assertEquals(s, "-2");
+	}
+	
+	
+	@Test
+//  @Disabled
+//  @DisplayName("right")
+	public void right() {
+		LoginService serivce=new LoginService();
+		String email = "123";
+		String password = "123";
+
+		String s = serivce.login(email, password);
+		assertEquals((s!="-1")&&(s!="-2"), true);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
