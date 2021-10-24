a5c20f446d56bac34491d8da27c00159cea84fb6
jason-gx1989 <jason-gx1989@users.noreply.github.com>
2021-10-25 02:31:56 +1100

    approvers.

---
+++ src/main/java/com/wemccm/common/pojo/ApproverDataPojo.java
@@ -0,0 +1,19 @@
+package com.wemccm.common.pojo;
+
+import java.util.List;
+
+import com.wemccm.common.entity.Approver;
+
+public class ApproverDataPojo extends ProjectBasicPojo {
+
+	private List<Approver> projectData;
+
+	public List<Approver> getProjectData() {
+		return projectData;
+	}
+
+	public void setProjectData(List<Approver> projectData) {
+		this.projectData = projectData;
+	}
+
+}
