94f24f64614a0b30aae7da601a8961fbdba3f259
909858512@qq.com <909858512@qq.com>
2021-10-25 12:11:41 +0800

    cs

---
+++ src/main/java/com/wemccm/common/pojo/NegotiatedConnectionData.java
@@ -0,0 +1,21 @@
+package com.wemccm.common.pojo;
+
+import java.util.List;
+
+import com.wemccm.common.entity.NegotiatedConnection;
+
+public class NegotiatedConnectionData   extends ProjectBasicPojo {
+	private NegotiatedConnection projectData;
+
+	public NegotiatedConnection getProjectData() {
+		return projectData;
+	}
+
+	public void setProjectData(NegotiatedConnection projectData) {
+		this.projectData = projectData;
+	}
+
+
+	
+}
+
